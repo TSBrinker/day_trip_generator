9616e954fc6e83e078c62e46b84c0d36f2c78fd2
TSBrinker <tsbrinker@gmail.com>
2022-09-16 15:36:00 -0500

    Cleaned up name capture, otherwise saving progress

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ def transport_select():
     transportation = random.choice(transports)
     return transportation
 
+# this was barely conceived when you popped in for a chat - probably still rough. Please ignore.
+# def transport_approve():
+    # approval = input(f"{user}, if you're going on a trip, we think you should take {transportation}. Sound good to you? y or n: ")
+    # while approval == 'n':
+    #     transports.remove(transportation)
+    #     approval = input(f"Okay, {user}, let's try again. How about {transportation}? y or n: ")
+
 def destination_select():
     destination = random.choice(destinations)
     return destination
@@ -30,13 +37,27 @@ def fun_select():
     return fun
 
 user = name_capture()
-sleep(3)
+
 
 def generation():
     transport = transport_select()
     destination = destination_select()
     food = food_select()
     fun = fun_select()
-    print(f"Alright, {user}! We think you should take {transport} to {destination}. While you're there, wanna grab {food} and hit {fun}?")
+
+def confirmation():
+    approval = ""
+    while approval == "" or approval == "n":
+        generation()
+        print(f"We think you should take {transport} to {destination}. While you're there, you should grab {food} and enjoy {fun}. Sound good? y or n: ")
+
 
 generation()
+confirmation()
+
+# def confirmation():
+#     approval = ""
+#     while approval == "" or approval == "n":
+#         generation()
+#     else:
+#         print('Great! To confirm: You will take ')
---
+++ outline.md
@@ -0,0 +1,35 @@
+Let's make a plan-
+
+Final Vision
+
+Transportation:
+    Plane
+    Train
+    Automobile
+
+Destination:
+    Des Moines
+    Chicago
+    Minneapolis
+    St. Paul
+    Omaha
+    Kansas City
+
+    To get it working, let's just do generic restaurants and entertainment for each.
+    Finalized, let's give each destination 3+ Restaurants and 3+ entertainments specific to the destination
+
+Itinerary will be returned for user approval
+If yes, user will be thanked
+If no, user will be asked which aspect of their trip they'd like to change
+
+Vetoed choice will be removed from the pool, and a new itinerary will be presented.
+Rinse and repeat.
+
+QUESTION: Present choices one at a time? or "here's a list, what don't you like?"
+
+
+
+Fine tuning if I get to it:
+Delays in input
+Characterization of the program ("Hmm, let me think..." etc.)
+
