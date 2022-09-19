3a7bb7241400a5a1df9ef75a013ea6e9ff67d1b4
TSBrinker <tsbrinker@gmail.com>
2022-09-19 17:08:46 -0500

    Finished generator 2.0, ready for submission!

--- generator 1.0.py
+++ generator 1.0.py
@@ -1,74 +1,19 @@
 import random
-import time
-from time import sleep
 
-destinations = ['Des Moines', 'Chicago', 'Minneapolis', 'St. Paul', 'Omaha', 'Kansas City', 'Milwaukee', 'Madison', 'LaCrosse']
 transports = ['a plane', 'a train', 'a bus', 'your vehicle', 'a taxi', 'an Uber']
+destinations = ['Des Moines', 'Chicago', 'Minneapolis', 'St. Paul', 'Omaha', 'Kansas City', 'Milwaukee', 'Madison', 'LaCrosse']
 restaurants = ['Applebees', 'Red Lobster', "Wendy's", "Raising Cane's", 'Olive Garden']
 entertainment_options = ['the zoo', 'the clubs', 'a park', 'a museum', 'a concert', 'a game', 'a theme park', 'the sights']
-
-# print('Hello, User!')
-# user = input('What is your name? ')
 
 def randomizer():
     selections = [random.choice(transports), random.choice(destinations), random.choice(restaurants), random.choice(entertainment_options)]
     return selections
-
-# def transport_select():
-#     transportation = random.choice(transports)
-#     return transportation
-
-# this was barely conceived when you popped in for a chat - probably still rough. Please ignore.
-# def transport_approve():
-    # approval = input(f"{user}, if you're going on a trip, we think you should take {transportation}. Sound good to you? y or n: ")
-    # while approval == 'n':
-    #     transports.remove(transportation)
-    #     approval = input(f"Okay, {user}, let's try again. How about {transportation}? y or n: ")
-
-# def destination_select():
-#     destination = random.choice(destinations)
-#     return destination
-
-# def food_select():
-#     food = random.choice(restaurants)
-#     return food
-
-# def fun_select():
-#     fun = random.choice(entertainment_options)
-#     return fun
-
-# def generation():
-#     approval = " "
-#     while approval != 'y':
-#         transport = transport_select()
-#         destination = destination_select()
-#         food = food_select()
-#         fun = fun_select()
-#         approval = input(f"We think you should take {transport} to {destination}. While you're there, you should grab {food} and enjoy {fun}. Sound good? y or n: ")
-#     else: 
-#         print(f"Great choice! You're going to love taking {transport} to {destination}, and we hope you'll enjoy {food} and {fun}!")
 
 def generation():
     approval = " "
     while approval != 'y':
         selections = randomizer()
         approval = input(f"We think you should take {selections[0]} to {selections[1]}. While you're there, you should grab {selections[2]} and enjoy {selections[3]}. Sound good? y or n: ")
-    else: 
-        print(f"Great choice! You're going to love taking {selections[0]} to {selections[1]}, and we hope you'll enjoy {selections[2]} and {selections[3]}!")
-
+    print(f"Great choice! You're going to love taking {selections[0]} to {selections[1]}, and we hope you'll enjoy {selections[2]} and {selections[3]}!")
 
 generation()
-
-# def confirmation():
-#     approval = ""
-#     while approval == "" or approval == "n":
-#         generation()
-#         print(f"We think you should take {transport} to {destination}. While you're there, you should grab {food} and enjoy {fun}. Sound good? y or n: ")
-
-
-# def confirmation():
-#     approval = ""
-#     while approval == "" or approval == "n":
-#         generation()
-#     else:
-#         print('Great! To confirm: You will take ')
--- generator 2.0.py
+++ generator 2.0.py
@@ -2,73 +2,51 @@ import random
 import time
 from time import sleep
 
-destinations = ['Des Moines', 'Chicago', 'Minneapolis', 'St. Paul', 'Omaha', 'Kansas City', 'Milwaukee', 'Madison', 'LaCrosse']
 transports = ['a plane', 'a train', 'a bus', 'your vehicle', 'a taxi', 'an Uber']
-restaurants = ['Applebees', 'Red Lobster', "Wendy's", "Raising Cane's", 'Olive Garden']
+destinations = ['Des Moines', 'Chicago', 'Minneapolis', 'St. Paul', 'Omaha', 'Kansas City', 'Milwaukee', 'Madison', 'LaCrosse']
+restaurants = ['Applebees', 'Red Lobster', "Wendy's", "Raising Cane's", 'Olive Garden', 'a buffet', 'a taco truck', 'a BBQ joint', 'a literal fish, still alive']
 entertainment_options = ['the zoo', 'the clubs', 'a park', 'a museum', 'a concert', 'a game', 'a theme park', 'the sights']
 
-# print('Hello, User!')
-# user = input('What is your name? ')
+def update_list(article, from_list):
+    from_list.remove(article)
 
-def randomizer():
-    selections = [random.choice(transports), random.choice(destinations), random.choice(restaurants), random.choice(entertainment_options)]
-    return selections
+def restore_original_list(main_list, refill_list):
+    if len(main_list) == 0:
+        main_list = refill_list.copy()
+        print('We ran out of options, bud. Let\'s start over, shall we?')
+    return main_list
 
-# def transport_select():
-#     transportation = random.choice(transports)
-#     return transportation
-
-# this was barely conceived when you popped in for a chat - probably still rough. Please ignore.
-# def transport_approve():
-    # approval = input(f"{user}, if you're going on a trip, we think you should take {transportation}. Sound good to you? y or n: ")
-    # while approval == 'n':
-    #     transports.remove(transportation)
-    #     approval = input(f"Okay, {user}, let's try again. How about {transportation}? y or n: ")
-
-# def destination_select():
-#     destination = random.choice(destinations)
-#     return destination
-
-# def food_select():
-#     food = random.choice(restaurants)
-#     return food
-
-# def fun_select():
-#     fun = random.choice(entertainment_options)
-#     return fun
-
-# def generation():
-#     approval = " "
-#     while approval != 'y':
-#         transport = transport_select()
-#         destination = destination_select()
-#         food = food_select()
-#         fun = fun_select()
-#         approval = input(f"We think you should take {transport} to {destination}. While you're there, you should grab {food} and enjoy {fun}. Sound good? y or n: ")
-#     else: 
-#         print(f"Great choice! You're going to love taking {transport} to {destination}, and we hope you'll enjoy {food} and {fun}!")
-
-def generation():
-    approval = " "
+def item_selection(list, prompt):
+    working_list = list.copy()
+    selection = random.choice(working_list)
+    approval = input(f"How about {prompt} {selection}. Sound good to you? y or n: ")
     while approval != 'y':
-        selections = randomizer()
-        approval = input(f"We think you should take {selections[0]} to {selections[1]}. While you're there, you should grab {selections[2]} and enjoy {selections[3]}. Sound good? y or n: ")
-    else: 
-        print(f"Great choice! You're going to love taking {selections[0]} to {selections[1]}, and we hope you'll enjoy {selections[2]} and {selections[3]}!")
+        working_list.remove(selection)
+        working_list = restore_original_list(working_list, list)
+        selection = random.choice(working_list)
+        sleep(.25)
+        print('Hmmm...')
+        sleep(1)
+        approval = input(f"How about {selection}? y or n: ")
+    print(f'It\'s decided then, you\'ll be {prompt} {selection}!')
+    sleep(.1)
+    return selection
 
+def full_generator():
+    finalized_choices = []
+    approval = ''
+    while approval != 'y':
+        finalized_choices.clear()
+        print('Welcome! Let\'s see what we can plan for you...')
+        sleep(.5)
+        finalized_choices.append(item_selection(transports, 'taking'))
+        finalized_choices.append(item_selection(destinations, 'visiting'))
+        finalized_choices.append(item_selection(restaurants, 'eating at'))
+        finalized_choices.append(item_selection(entertainment_options, 'enjoying'))
+        approval = input(f'Okay, great! So you\'d like to take {finalized_choices[0]} to {finalized_choices[1]}, and while you\'re there you\'ll be eating at {finalized_choices[2]}, and enjoying {finalized_choices[3]}. Ready to lock in your selections? y or n: ')
+        print(f"Wonderful! To recap, you're taking {finalized_choices[0]} to {finalized_choices[1]}, {finalized_choices[2]} and {finalized_choices[3]} when you get there.")
+    sleep(1)
+    print()
+    print("Thank you so much for using our generator! Enjoy your day trip!")
 
-generation()
-
-# def confirmation():
-#     approval = ""
-#     while approval == "" or approval == "n":
-#         generation()
-#         print(f"We think you should take {transport} to {destination}. While you're there, you should grab {food} and enjoy {fun}. Sound good? y or n: ")
-
-
-# def confirmation():
-#     approval = ""
-#     while approval == "" or approval == "n":
-#         generation()
-#     else:
-#         print('Great! To confirm: You will take ')
+full_generator()
